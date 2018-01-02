2b66fbb8b848a1df44944c16f296869fe0ab01d4
Jinya Chen <jinyachen@gmail.com>
2018-01-02 21:32:07 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * index.js
+ */
+const colorful = require('./colorful');
+
+colorful("Server use: npm run server", 'info', true);
+colorful("Client use: npm run client \"your command\"", 'info', true);
+
