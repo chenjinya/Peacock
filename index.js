69d3340f18840d75ef26b9cc7f612505473d71e0
Jinya Chen <jinyachen@gmail.com>
2018-01-03 17:57:04 +0800

    update notice

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 const colorful = require('./colorful');
 
-colorful("This module should not be loaded by module", 'notice', true);
+colorful("This program should not be loaded by module", 'notice', true);
 colorful("Server use: npm run server", 'info', true);
 colorful("Client use: npm run cmd \"your command\"", 'info', true);
 
