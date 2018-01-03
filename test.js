4ab888aa5c143d9ffa63ca033e57901c054d44ae
Jinya Chen <jinyachen@gmail.com>
2018-01-03 16:33:29 +0800

    test

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ execServerProcess = exec('npm run server', (err, stdout, stderr) => {
     if(err || stderr) {
         execClientProcess && execClientProcess.kill();
         execServerProcess && execServerProcess.kill();
-        console.log("Test fail!");
+        console.log("Server Test fail!");
         process.exit(1);
     }
     console.log(stdout);
@@ -18,7 +18,7 @@ execClientProcess = exec('npm run cmd "echo success"', (err, stdout, stderr) => 
     if(err || stderr) {
         execClientProcess && execClientProcess.kill();
         execServerProcess && execServerProcess.kill();
-        console.log("Test fail!");
+        console.log("Client Test fail!");
         process.exit(1);
     } else {
         console.log(stdout);
